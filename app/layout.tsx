e6970198e3a1943883f5869b90f5051e6244c4be
noe campo <noecampo@delosintelligence.fr>
2025-03-24 21:10:49 +0100

    jfoze

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,8 +14,8 @@ const ubuntu = Ubuntu({
 });
 
 export const metadata: Metadata = {
-  title: "essec-gpt",
-  description: "L'assistant alumni IA de l'essec",
+  title: "KEDGE-gpt",
+  description: "L'assistant alumni IA de l'KEDGE",
 };
 
 export default function RootLayout({
